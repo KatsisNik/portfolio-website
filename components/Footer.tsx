3d17aac746d14f1e369474aae17545dff13cd113
katsisnik <katsisnik@gmail.com>
2024-09-10 15:15:31 +0100

    Fixed minor bugs

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import { socialMedia } from "@/data"
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
+    <footer className="w-full mb-[100px] pb-10 md:mb-5" id="contact">
         <div className="w-full absolute left-0 -bottom-72 min-h-96">
             <img 
             src="/footer-grid.svg"
@@ -30,21 +30,19 @@ const Footer = () => {
         </div>
 
         <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
-            <p className="md:text-basxt-sm md:font-normal font-light">Copyright © 2024 Nikolaos Katsis</p>
-            <div className="flex-items-center md:gap-3 gap-6">
-                {socialMedia.map((profile) => (
-                    <div key={profile.id} className="w-10 h-10 curser-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
-                        <img 
-                            src={profile.img}
-                            alt={profile.img}
-                            width={20} height={20}
-                        />
-                    </div>
-                ))}
-
+            <p className="md:text-base text-sm md:font-normal font-light">Copyright © 2024 Nikolaos Katsis</p>
+            
+            <div className="flex items-center md:gap-3 gap-6">
+          {socialMedia.map((info) => (
+            <div
+              key={info.id}
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+            >
+              <img src={info.img} alt="icons" width={20} height={20}/>
             </div>
+          ))}
         </div>
-
+      </div>
     </footer>
   )
 }
